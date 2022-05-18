4b0bb03a5a65c4f1aada48035aa2d35e3583820e
bws.ksi <bws.ksi@gmail.com>
2022-05-18 10:02:07 +0300

    update

---
+++ script.js
@@ -0,0 +1,25 @@
+// recursion
+
+function GCD(a, b) {
+  if (!b) {return a;}
+  return GCD(b, a % b);  
+}
+
+console.log(GCD(462, 1071));
+
+
+// binary
+function GCD2 (c,d) {
+  if(a == 0) {
+    return b;
+  }
+  while (b != 0 ) {
+    if (a > b) {
+      a = a - b;
+    } else {
+      b = b - a;
+    }
+  }
+  return a;
+}
+console.log(GCD(100 , 20));
